92280c1fae6d45ba1e154c6608849694d28b7876
shaamas-khan <sktanoli420@gmail.com>
2024-04-26 14:15:19 +0500

    first commit

---
+++ src/hook/useApi.js
@@ -0,0 +1,29 @@
+import { useState, useEffect,useCallback,memo } from "react";
+
+ export const useApi =(url) => {
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(false);
+console.log("useapi hook");
+  const fetchData = useCallback(async () => {
+    try {
+      const response = await fetch(url);
+      if (!response.ok) throw new Error("Failed to fetch data");
+      const json = await response.json();
+      setData(json);
+    } catch (err) {
+      setError(err);
+    } finally {
+      setLoading(false);
+    }
+  },[url]);
+  useEffect(() => {
+    setError(false);
+    fetchData();
+  }, [url,fetchData ]);
+
+  return [loading, data, error]  ;
+};
+
+  
+   
---
+++ src/pages/Home.jsx
@@ -0,0 +1,39 @@
+import { useApi } from "../hook/useApi";
+
+const Home = () => {
+  const [loading, data, error] = useApi("https://dummyjson.com/products");
+  console.log("home");
+  const { products } = data;
+  console.log(products);
+  return (
+    <>
+      <div className="container mx-auto ">
+        <div className="flex flex-wrap  ">
+          {products?.map((product) => (
+            <div
+              key={product.id}
+              className="card w-[23%] bg-slate-800 shadow-xl mx-2 my-4"
+            >
+              <figure className="px-10 pt-10">
+                <img
+                  src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+                  alt="Shoes"
+                  className="rounded-xl"
+                />
+              </figure>
+              <div className="card-body items-center text-center">
+                <h2 className="card-title">Shoes!</h2>
+                <p>If a dog chews shoes whose shoes does he choose?</p>
+                <div className="card-actions">
+                  <button className="btn btn-primary">ADD TO CART</button>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
